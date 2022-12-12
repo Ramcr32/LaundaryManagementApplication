b3261b5ba24ddd3862463d001bf2c0f021a12a67
Chova Ram Sinha <75615469+Ramcr32@users.noreply.github.com>
2022-12-12 15:48:57 +0530

    created : implements customer and employee service and controller

---
+++ LaundaryManagingApplication/src/main/java/com/laundaryApplication/LaundaryManagingApplication/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.laundaryApplication.LaundaryManagingApplication.controller;
+
+import com.laundaryApplication.LaundaryManagingApplication.model.Employee;
+import com.laundaryApplication.LaundaryManagingApplication.service.EmpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Employee")
+public class EmployeeController {
+    @Autowired
+    private EmpService empService;
+
+    @PostMapping("/")
+    public ResponseEntity<Employee> createNewEmpHandler(@RequestBody Employee emp){
+        Employee employee = empService.createEmployee(emp);
+        return  new ResponseEntity<>(employee, HttpStatus.ACCEPTED);
+
+    }
+    @PutMapping("/")
+    public ResponseEntity<Employee> updateEmpHandler(@RequestBody Employee emp){
+        Employee employee = empService.updateEmployee(emp);
+        return new ResponseEntity<>(employee,HttpStatus.OK);
+    }
+    @DeleteMapping("/empId")
+    public ResponseEntity<Employee> updateEmpHandler(@PathVariable Integer empId){
+        Employee employee = empService.deleteEmployee(empId);
+        return new ResponseEntity<>(employee,HttpStatus.OK);
+    }
+
+}
